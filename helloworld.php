802494105d41cd2e07bdb68c92dda92be3281211
askaks <askaks@greeny.cs.tlu.ee>
2016-02-11 12:53:32 +0200

    Added more code

--- helloworld.php
+++ helloworld.php
@@ -11,4 +11,21 @@
 	// For adding up strings . sign
 	echo $first_name." ".$last_name;
 
+	// > greater than
+	// < smaller than
+	// >= greater than or equal
+	// <= smaller than or equal
+	// == equal
+	// != not equal
+	
+	$age = 5;
+	
+	//if age is smaller than or equal to 6, then you can sleep at lunchtime
+	if($age <=6){
+		//if true
+		echo " Yes you can sleep at lunchtime";
+	}else{
+		//if false
+		echo " No, you are at school";
+	}
 ?>
